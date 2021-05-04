3075f810266b1097db21399f706c6ce58e2136b4
Juan C. Solano <jcsolanob@unal.edu.co>
2021-05-04 09:58:28 -0500

    Create index.py

---
+++ index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+welcome = "<center><h1>Mi primer programa</h1><br>" # Defining header
+message = "Hola Misi&oacute;n TIC 2022 <br>" # Welcome message
+studentname = "Soy Juan Camilo Solano</center>" # Defining student name
+
+print welcome, message, studentname
+
+exit()
