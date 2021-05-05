a53fcacf9660fb54c9815e0dcc568d64a008cd70
Juan C. Solano <jcsolanob@unal.edu.co>
2021-05-04 19:58:49 -0500

    Update index.py

--- index.py
+++ index.py
@@ -4,6 +4,6 @@ welcome = "<center><h1>Mi primer programa</h1><br>" # Defining header
 message = "Hola Misi&oacute;n TIC 2022 <br>" # Welcome message
 studentname = "Soy MMM</center>" # Defining student name
 
-print welcome, message, studentname
+print welcome, message, studentname, "<hr>"
 
 exit()
