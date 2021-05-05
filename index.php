7ae407858c1c5459a26c9dfe367ac66207387706
Juan C. Solano <jcsolanob@unal.edu.co>
2021-05-04 19:59:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php
 
-$python = `python index.py`;
-echo $python;
+$index = `python index.py`;
+echo $index;
+
+$S2 = `python S2.py`;
+echo $S2;
+
+$S3 = `python S3.py`;
+echo $S3;
+
+$S4 = `python S4.py`;
+echo $S4;
+
+$S5 = `python S5.py`;
+echo $S5;
+
+$S6 = `python S6.py`;
+echo $S6;
 
 ?>
