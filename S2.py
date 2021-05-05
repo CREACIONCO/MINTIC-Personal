a61257c6bfaa87c1f0b6e7779289d6327d25114e
Juan C. Solano <jcsolanob@unal.edu.co>
2021-05-04 20:00:28 -0500

    Create S2.py

---
+++ S2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+br = "<br>"
+hr = "<hr>"
+
+print "<h1>Clase 2021-05-04</h1>", br, "Hello, I'm Juan", br
+
+my_char = "a"
+print my_char, br
+
+my_string = "Juan Camilo"
+print my_string, br
+
+my_int = 5
+print my_int, br
+
+my_float = 5.8
+print my_float, br
+
+my_round = int (my_float)
+print my_round, br
+
+my_float = float (my_int)
+print my_float, br
+
+my_boolean_1 = True
+my_boolean_2 = False
+my_boolean_1 = int (False)
+print my_boolean_1, br
+
+print hr
