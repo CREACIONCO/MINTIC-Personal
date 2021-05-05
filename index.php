f306f38cec64e4bc565ee8dead1ebaf8717eb180
Juan C. Solano <jcsolanob@unal.edu.co>
2021-05-05 07:41:09 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,26 @@
 <?php
 
-$index = `python index.py`;
-echo $index;
+$first_program = `python first_program.py`;
+echo "<h1>First program</h1></br>";
+echo $first_program;
+echo "<hr>";
 
-$S2 = `python S2.py`;
-echo $S2;
+$classes_journal = `python classes journal.py`;
+echo "<h1>Classes Journal</h1></br>";
+echo $classes_journal;
+echo "<hr>";
 
 $S3 = `python S3.py`;
 echo $S3;
+echo "<hr>";
 
 $S4 = `python S4.py`;
 echo $S4;
+echo "<hr>";
 
 $S5 = `python S5.py`;
 echo $S5;
+echo "<hr>";
 
 $S6 = `python S6.py`;
 echo $S6;
