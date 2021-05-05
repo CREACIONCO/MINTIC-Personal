0b315527f0faf117a5ffc63a0e8d7792e3a6740d
Juan C. Solano <jcsolanob@unal.edu.co>
2021-05-05 07:43:53 -0500

    Update and rename index.py to first_program.py

---
+++ first_program.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+message = "Hola Misi&oacute;n TIC 2022 <br>" # Welcome message
+studentname = "Soy Pepito" # Defining student name
+
+print welcome, message, studentname
+
+exit()
--- index.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-welcome = "<center><h1>Mi primer programa</h1><br>" # Defining header
-message = "Hola Misi&oacute;n TIC 2022 <br>" # Welcome message
-studentname = "Soy MMM</center>" # Defining student name
-
-print welcome, message, studentname, "<hr>"
-
-exit()
