ab1744bf301f9e29f5db0b101d0701f23cf15b96
Juan C. Solano <jcsolanob@unal.edu.co>
2021-05-05 19:40:16 -0500

    Update classes_journal.py

--- classes_journal.py
+++ classes_journal.py
@@ -28,3 +28,11 @@ my_boolean_1 = int (False)
 print my_boolean_1, br
 
 print true != 0, br
+
+print "<h2>Clase 2021-05-05</h2><br>"
+
+first_name = input("Input your first name: ")
+last_name = input ("Input your last name: ")
+email = input ("Input your email address: ")
+address = input ("Input your address: ")
+print(f"First name: {first_name}{br}Last name:{last_name}{br}Email: {email}{br}Address: {address}")
